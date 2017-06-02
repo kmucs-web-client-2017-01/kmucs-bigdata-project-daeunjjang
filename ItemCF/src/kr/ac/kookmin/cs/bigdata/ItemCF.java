a92bab9193b9160ce081e0bfcf95a6a07a0440f7
DaEun Kim <ekdms717@kookmin.ac.kr>
2017-06-02 18:19:19 +0900

    add code getting average of overall about asin

---
+++ ItemCF/src/kr/ac/kookmin/cs/bigdata/ItemCF.java
@@ -0,0 +1,90 @@
+package kr.ac.kookmin.cs.bigdata;
+
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+
+public class ItemCF extends Configured implements Tool {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Arrays.toString(args));
+        int res = ToolRunner.run(new Configuration(), new ItemCF(), args);
+      
+        System.exit(res);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        System.out.println(Arrays.toString(args));
+
+        Job job = Job.getInstance(getConf());
+        job.setJarByClass(ItemCF.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+      
+        return 0;
+    }
+   
+    public static class Map extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+        private final static DoubleWritable ONE = new DoubleWritable(1);
+        private Text word = new Text();
+
+        @Override
+        public void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+            for (String token: value.toString().split("\n")) {
+            	try{
+            		JSONObject jsonObject = new JSONObject(token);
+            		word.set((String) jsonObject.get("asin"));
+            		context.write(word, new DoubleWritable(jsonObject.getDouble("overall")));
+            	}
+              catch (JSONException j) {
+              j.printStackTrace();
+              }
+            }
+            }
+        }
+
+    public static class Reduce extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+        @Override
+        public void reduce(Text key, Iterable<DoubleWritable> values, Context context) 
+        		throws IOException, InterruptedException {
+        	double sum = 0;
+            int cnt = 0;
+            for (DoubleWritable val : values) {
+                sum += val.get();
+                cnt ++;
+            }
+            
+            context.write(key, new DoubleWritable(sum/cnt));
+        
+        }
+    }
+}
